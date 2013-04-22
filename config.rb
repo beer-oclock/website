8c84a8b05769b913438ba8d6cb96abdbbb170b07
James White <dev.jameswhite@gmail.com>
2013-04-23 00:39:09 +0100

    Oops

--- config.rb
+++ config.rb
@@ -1,5 +1,3 @@
-# Require any additional compass plugins here.
-
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "styles/css"
@@ -15,10 +13,3 @@ relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
-
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
