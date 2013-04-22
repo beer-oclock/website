689ac012e79dcfd47007c5eb6de338290f33362f
benhodgson87 <ben@someaddress.net>
2013-04-23 00:03:06 +0100

    Moved CSS and Sass into separate folders
    - Updated Compass config
    - Updated folder path structure in _base
    - Updated location of main stylesheet in index.html

--- config.rb
+++ config.rb
@@ -2,8 +2,8 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
-sass_dir = "css"
+css_dir = "styles/css"
+sass_dir = "styles/sass"
 images_dir = "img"
 javascripts_dir = "js"
 
